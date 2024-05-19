890cb341f08e7be82cc163cc9e5fc7bfd6be7c25
TrucNguyen2211 <s3974820@rmit.edu.vn>
2024-05-19 10:49:45 +0700

    fix bug

--- frontend/src/Pages/OtherUserProfile/components/PostCard.jsx
+++ frontend/src/Pages/OtherUserProfile/components/PostCard.jsx
@@ -68,10 +68,10 @@ function PostCard({ post, onToggleLike, handleLockConfirm, handleUnLockConfirm }
                             <Row>
                                 <Col className="align-items-center">
                                     <Card.Title><strong>{post.title}</strong></Card.Title>
-                                    <Card.Subtitle className="mb-2 text-muted">@{post.authorId.username}</Card.Subtitle>
+                                    <Card.Subtitle className="mb-2 text-muted">@{post.authorId?.username}</Card.Subtitle>
                                     <Card.Text><small>{new Date(post.createdAt).toLocaleString()}</small></Card.Text>
                                 </Col>
-                                {(post.authorId._id === user?._id || user?.role === 'admin') && (
+                                {(post.authorId?._id === user?._id || user?.role === 'admin') && (
                                     <Col xs="auto" className="align-items-end">
                                         <Dropdown>
                                             <Dropdown.Toggle
